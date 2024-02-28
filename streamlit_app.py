198723730bf0703dc55d0538f5b210f0e448647c
Eren Taymaz <58527761+erentaymaz@users.noreply.github.com>
2024-02-28 20:41:11 +0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Title of the application
+st.title("My Streamlit App")
+
+# Header
+st.header("Welcome to my app!")
+
+# Subheader
+st.subheader("This is a demo of Streamlit")
